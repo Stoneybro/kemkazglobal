1a7a9fd7e616f66d4b9799de910291d98fb3c3c5
Zion livingstone <zionlivingstone4@gmail.com>
2023-07-13 21:18:40 +0100

    x

--- src/components/text.jsx
+++ src/components/text.jsx
@@ -4,7 +4,7 @@ const Text = () => {
   return (
     <div className='w-full'>
         <div className=" bg-[#D9EBFF]   ">
-            <div className="lg:w-[950px]  text-xl lg:text-2xl mx-auto py-16 lg:py-32 px-8 text-center leading-normal font-semibold">
+            <div className="lg:w-[950px]  text-xl lg:text-2xl mx-auto py-16 lg:py-32 px-8 text-center leading-normal font-medium">
             With a comprehensive range of services encompassing logistics, business services consulting, real estate, investment, and more, our company seamlessly integrates diverse solutions and leverages extensive global knowledge to empower businesses in achieving their importation, exportation, and other goals, driving their overall success.
             </div>
         </div>
