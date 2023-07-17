4c327e84e3a097b1d422cf3f6aa9ce1de44c4bdb
Zion livingstone <zionlivingstone4@gmail.com>
2023-07-17 20:33:00 +0100

    x

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { FaWhatsapp,FaPhone, } from 'react-icons/fa'
 import {HiOutlineMail,HiOutlineLocationMarker} from 'react-icons/hi'
@@ -6,17 +7,21 @@ import {BsTelephone} from 'react-icons/bs'
 
 
 const Contact = () => {
+    const emailbutton=()=>{
+        const mailtoUrl='mailto:kemkazglobal@gmail.com, little4reading@gmail.com';
+        window.location.href=mailtoUrl
+    }
   return (
     <div className='py-16'>
         <div className="flex items-center justify-center flex-col ">
        <div className="text-5xl">Contact us</div>
        <div className="text-sm text-[#575455] mb-8 lg:mb-12">let us know how we can help you</div>
         <div className="flex gap-4 flex-wrap px-8 lg:px-4 items-center justify-center">
-            <div className="border border-gray-400 after:bg-primary border-opacity-25 p-4  w-full lg:w-56 flex flex-col items-start  gap-4 card relative">
+            <button onClick={emailbutton} className="border border-gray-400 after:bg-primary border-opacity-25 p-4  w-full lg:w-56 flex flex-col items-start  gap-4 card relative">
                 <div className="">Email us</div>
                 <div className="text-xs mb-1 text-[#575455]">send us a message.</div>
-                <div className="text-xs font-semibold underline">kemkaz@gmail.com</div>
-            </div>
+                <div className="text-xs font-semibold underline">kemkazglobal@gmail.com ,little4reading@gmail.com</div>
+            </button>
             <div className="border after:bg-[#F75C4E] border-opacity-25 p-4  w-full lg:w-56 flex flex-col items-start  gap-4 border-gray-400 card relative">
            <div className=""> Chat with us</div>
            <div className="text-xs mb-1 text-[#575455]">speak to our friendly team</div>
@@ -30,7 +35,7 @@ const Contact = () => {
             <div className="border after:bg-[#F7954E] border-opacity-25 p-4  w-full lg:w-56 flex flex-col items-start  gap-4 border-gray-400 card relative">
             <div className="">Call us</div>
                 <div className="text-xs mb-1 text-[#575455]">Mon-Fri from 8am-5pm</div>
-                <div className="text-xs font-semibold underline">+1(555) 000-0000</div>
+                <div className="text-xs font-semibold underline">+1(347) 283-3784</div>
             </div>
         </div>
 
