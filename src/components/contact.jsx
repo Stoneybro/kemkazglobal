940fd862c034bd2fd861fdbe6beebacb1bd216f7
Zion livingstone <zionlivingstone4@gmail.com>
2023-07-17 23:02:31 +0100

    x

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -22,6 +22,11 @@ const Contact = () => {
         window.open(mapUrl , '_blank')
         
     }
+    const chatbutton=()=>{
+        const mapUrl=`https://wa.me/13472833784`
+        window.open(mapUrl , '_blank')
+        
+    }
     
   return (
     <div className='py-16'>
@@ -32,13 +37,13 @@ const Contact = () => {
             <button onClick={emailbutton} className="border border-gray-400 after:bg-primary border-opacity-25 p-4  w-full lg:w-56 flex flex-col items-start  gap-4 card relative">
                 <div className="">Email us</div>
                 <div className="text-xs mb-1 text-[#575455]">send us a message.</div>
-                <div className="text-left text-xs font-semibold underline ">kemkazglobal@gmail.com ,little4reading@gmail.com</div>
+                <div className="text-left text-xs font-semibold underline ">kemkazglobal@gmail.com</div>
             </button>
-            <div className="border after:bg-[#F75C4E] border-opacity-25 p-4  w-full lg:w-56 flex flex-col items-start  gap-4 border-gray-400 card relative">
+            <button className="border after:bg-[#F75C4E] border-opacity-25 p-4  w-full lg:w-56 flex flex-col items-start  gap-4 border-gray-400 card relative">
            <div className=""> Chat with us</div>
            <div className="text-xs mb-1 text-[#575455]">speak to our friendly team</div>
-           <div className="text-left text-xs font-semibold underline">09039093290</div>
-            </div>
+           <div className="text-left text-xs font-semibold underline">+1(347) 283-3784</div>
+            </button>
             <button onClick={mapbutton} className="border after:bg-[#5C4EF7] border-opacity-25 p-4  w-full lg:w-56 flex flex-col items-start  gap-4 border-gray-400 card relative">
             <div className="">Visit us</div>
                 <div className="text-xs mb-1 text-[#575455]">18,Comstock ave staten Island NY 10314</div>
